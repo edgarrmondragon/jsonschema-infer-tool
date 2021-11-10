48af83be25d151883967acb46e6977fa582d84f4
Edgar Ramírez <edgarrm358@gmail.com>
2021-11-10 03:24:41 -0600

    Better variable name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
     let stdin = std::io::stdin();
-    let v = serde_json::from_reader(stdin).expect("error while reading");
-
-    let schema = infers_jsonschema::infer(&v);
+    let json_object = serde_json::from_reader(stdin).expect("error while reading");
+    let schema = infers_jsonschema::infer(&json_object);
     let pretty = serde_json::to_string_pretty(&schema).expect("couldn't prettify schema");
 
     println!("{}", pretty);
